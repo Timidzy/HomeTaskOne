a8b8baa84acfdcc3b20071189409e50ddd16234f
Timidzy <prystaukat@gmail.com>
2022-04-05 00:28:55 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+
+public static void main(String[] args) {
+
+Person tim = new Person();
+tim.name = "Tim";
+Person jill = new Person();
+jill.name = "Jill";
+System.out.println("Name of the first person is " + tim.name);
+System.out.println("Name of the second person is " + jill.name);
+
+}
+}
